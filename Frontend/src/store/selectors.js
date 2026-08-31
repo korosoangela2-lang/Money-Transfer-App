cb389da4a64d5d9af92653d54fc4c1a898ad4e7c
vivianwanjiku-lab <vivian.wanjiku@student.moringaschool.com>
2026-08-31 19:25:24 +0300

    Remove comment from corridorSplit function
    Removed outdated comment regarding corridorSplit function.

--- Frontend/src/store/selectors.js
+++ Frontend/src/store/selectors.js
@@ -31,7 +31,6 @@ export const select = {
     return [...buckets.values()];
   },
 
-  /** Where the money goes, by destination currency. */
   corridorSplit: (s) => {
     const map = new Map();
     select.sends(s).filter((t) => t.status !== "failed").forEach((t) => {
